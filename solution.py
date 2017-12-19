e6ce253d04a8e1cd4893f258e6cf5ee716caa609
Max Cabrera <cabrera.max@gmail.com>
2017-12-18 18:40:51 -0600

    added diagonal units

--- solution.py
+++ solution.py
@@ -6,9 +6,8 @@ row_units = [cross(r, cols) for r in rows]
 column_units = [cross(rows, c) for c in cols]
 square_units = [cross(rs, cs) for rs in ('ABC','DEF','GHI') for cs in ('123','456','789')]
 unitlist = row_units + column_units + square_units
-
-# TODO: Update the unit list to add the new diagonal units
-unitlist = unitlist
+diag_units = [[rows[i]+cols[i] for i in range(9)],[rows[i]+cols[-i-1] for i in range(9)]]
+unitlist = unitlist + diag_units
 
 units = dict((s, [u for u in unitlist if s in u]) for s in boxes)
 peers = dict((s, set(sum(units[s],[]))-set([s])) for s in boxes)
